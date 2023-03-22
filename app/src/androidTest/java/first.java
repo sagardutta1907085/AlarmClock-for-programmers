4e4779880fbbaf373026b06c66eb706903608fac
Sagar Dutta <sagardutta404@gmail.com>
2023-03-23 01:19:31 +0600

    commit

---
+++ app/src/androidTest/java/first.java
@@ -0,0 +1,24 @@
+public class first {
+    int a,b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public first(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
