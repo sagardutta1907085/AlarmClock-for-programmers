88bb14e069338aa1e5851f0a8c473a55f91cbb2a
Sagar Dutta <sagardutta404@gmail.com>
2023-03-23 01:47:54 +0600

    m

--- app/src/androidTest/java/first.java
+++ app/src/androidTest/java/first.java
@@ -1,6 +1,11 @@
 public class first {
     int a,d;
 
+    public first(int a, int d) {
+        this.a = a;
+        this.d = d;
+    }
+
     public int getA() {
         return a;
     }
@@ -14,11 +19,6 @@ public class first {
     }
 
     public void setD(int d) {
-        this.d = d;
-    }
-
-    public first(int a, int d) {
-        this.a = a;
         this.d = d;
     }
 }
